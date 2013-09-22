8ca0d7806f412bce80ed036fa3463742bb0cd235
RyanDur <ryan.p.durling@gmail.com>
2013-09-22 03:26:54 -0700

    pages

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "Pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/pages/home'
+      expect(page).to have_content('Survey')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/pages/about'
+      expect(page).to have_content('About')
+    end
+  end
+end
