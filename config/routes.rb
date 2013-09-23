4451cc629e3460f79efd1489f1272777eef9188b
RyanDur <ryan.p.durling@gmail.com>
2013-09-23 01:24:16 -0700

    users controller, signup_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Survey::Application.routes.draw do
+  get "users/new"
   root 'pages#home'
+  match 'signup', to: 'users#new', via: :get
   match 'help', to: 'pages#help', via: :get
   match 'about', to: 'pages#about', via: :get
   match 'contact', to: 'pages#contact', via: :get
